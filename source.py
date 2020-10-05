0f50202ae7fac3816af509c5789087fbb73bb8da
John Mikhailov <mikhailovj@gmail.com>
2020-10-05 23:39:01 +0300

    core

---
+++ source.py
@@ -0,0 +1,29 @@
+import os
+import dotenv
+
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+
+dotenv.load_dotenv()
+
+token = os.getenv('TOKEN')
+
+updater = Updater(token=token, use_context=True)
+dispatcher = updater.dispatcher
+
+
+def intro(update, context):
+    print('into intro')
+    context.bot.send_message(chat_id=update.effective_chat.id, text="That's where the fun begins")
+
+
+def reflector(update, context):
+    print('reflectors code')
+    context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
+
+
+start_handler = CommandHandler('start', intro)
+message_handler = MessageHandler(Filters.text & (~Filters.command), reflector)
+dispatcher.add_handler(start_handler)
+dispatcher.add_handler(message_handler)
+updater.start_polling()
+updater.idle()
