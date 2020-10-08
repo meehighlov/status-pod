a3ef7a06b986feb64a786d9d3e0ef1b530aca6de
John Mikhailov <mikhailovj@gmail.com>
2020-10-08 23:35:59 +0300

    start payments

--- source.py
+++ source.py
@@ -1,7 +1,11 @@
 import os
+import sqlite3
+
 import dotenv
+import datetime
 
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from apps.persistant.db import db
 
 dotenv.load_dotenv()
 
@@ -18,22 +22,38 @@ def intro(update, context):
 
 
 def reflector(update, context):
-    print(f'chat id {update.effective_chat.id}')
+    db.cursor.execute(''' select * from payments ''')
+    r = db.cursor.fetchall()
+    print(f'db data {r}')
     context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
 
 
-def callback(context):
-    context.bot.send_message(chat_id='69528108',
-                             text='notification')
-
+def payments(update, context):
+    text = update.message.text
+    try:
+        item, cost = text.split()
+        cost = float(cost)
+    except ValueError:
+        context.bot.send_message(chat_id=update.effective_chat.id, text='not saved')
+        return
+    connection = sqlite3.connect('temporary.persistant')
+    cursor = connection.cursor()
+    cursor.execute('''select * from payments;''')
+    r = cursor.fetchall()
+    print(f'db data {r}')
+    cursor.execute('''insert into payments values (?, ?, ?)''', (item, cost, 'today'))
+    connection.commit()
+    connection.close()
+    context.bot.send_message(chat_id=update.effective_chat.id, text='saved')
 
 
 start_handler = CommandHandler('start', intro)
-message_handler = MessageHandler(Filters.text & (~Filters.command), reflector)
+payments_handler = MessageHandler(Filters.text & (~Filters.command), payments)
+# message_handler = MessageHandler(Filters.text & (~Filters.command), reflector)
 
 # job_minute = queue.run_repeating(callback, interval=10, first=0)
 
 dispatcher.add_handler(start_handler)
-dispatcher.add_handler(message_handler)
+dispatcher.add_handler(payments_handler)
 updater.start_polling()
 updater.idle()
