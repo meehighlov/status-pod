f15952a78413cd58cb382caeb129175be0c6a628
John Mikhailov <mikhailovj@gmail.com>
2020-10-21 00:06:56 +0300

    wip: notifications

--- apps/notifications/functions.py
+++ apps/notifications/functions.py
@@ -13,20 +13,26 @@ def notification_fabric(*args, **kwargs):
     return notification
 
 
-"""
-    sample
+def add_notification_from_message(update, context):
+    # format: /na notification text date hh:mm
+    # day -> dd.mm
 
-    def message_handler(update, context):
-        notification_params =  context.args
-        message = notification_params.text
-        timer_options = notification_params.schedule
-        notification = notification_fabric(
-            chat_id=config.chat_id
-            message=message
-        )
-        context.job_queue.run_repeating(
-            notification,
-            interval=timer_options,
-            first=timer_options
-        )
-"""
+    notification_params = context.args
+
+    try:
+        day_, time_ = notification_params[-2:]
+
+    except (ValueError, IndexError):
+        pass
+
+    # message = notification_params.text
+    # timer_options = notification_params.schedule
+    # notification = notification_fabric(
+    #     chat_id=config.chat_id
+    #     message=message
+    # )
+    # context.job_queue.run_repeating(
+    #     notification,
+    #     interval=timer_options,
+    #     first=timer_options
+    # )
