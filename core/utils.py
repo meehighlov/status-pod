2f324b9cca14abacf602f07bb672fd37a48d4a9b
John Mikhailov <mikhailovj@gmail.com>
2020-11-05 18:12:04 +0300

    add converter days -> seconds

--- core/utils.py
+++ core/utils.py
@@ -1,2 +1,9 @@
 def strip_none_params(*args):
     return [param for param in args if param is not None]
+
+
+def get_seconds_by_days(days):
+    minute = 60
+    hour = minute * 60
+    day = hour * 24
+    return day * days
