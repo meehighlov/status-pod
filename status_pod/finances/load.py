9878f673f7e37f2aea55e768b456df86c8f67f52
John Mikhailov <mikhailovj@gmail.com>
2021-02-06 01:29:55 +0300

    fix index building

--- status_pod/finances/load.py
+++ status_pod/finances/load.py
@@ -26,7 +26,7 @@ def get_raw_file_data_as_matrix(path):
         return list(r)
 
 
-def get_headers(r: iter) -> t.List[t.Optional[t.List]]:
+def get_headers(r: iter) -> t.List[t.List]:
     """
     WARNING: modifies r
 
@@ -43,6 +43,11 @@ def get_headers(r: iter) -> t.List[t.Optional[t.List]]:
         return []
 
 
+def flatten_headers(headers):
+    """помним, что хедеров два"""
+    return [*headers[0], *headers[1]]
+
+
 def get_index_position_in_headers(index: str, headers: t.List[t.List]):
     # так как хедеров не один (сейчас 2) - ищем наш индекс в каждом из них
     for header in headers:
@@ -53,7 +58,7 @@ def get_index_position_in_headers(index: str, headers: t.List[t.List]):
     return None
 
 
-def map_index_to_its_position(indexes: t.Set[str], headers: t.List[t.List]):
+def map_column_name_to_its_position(indexes, headers):
     return {
         index: get_index_position_in_headers(index, headers)
         for index in indexes
@@ -73,23 +78,46 @@ def check_index_names_and_get_known(indexes: t.List[str], headers: t.List[t.List
             if known:
                 # TODO логировать то, что индекс будет строиться по только известным заголовкам
                 return known
-        raise LoadFinancesError(f'Unknown header names: {unknown}')
+        raise LoadFinancesError(f'Cannot create index: unknown header names: {unknown}')
     return known
 
 
+def process_row(row: t.List[str], name_to_position: dict):
+    return {
+        name: row[pos]
+        for name, pos in name_to_position.items()
+    }
+
+
 def build_index_on_raw_file_data(path, indexes: t.List[str] = None):
+    """
+    бахаем индекс только по уникальным значениям
+    потому что так проще и эффективнее работать с данными
+
+    понадобится больше значений под индексом - раскоментирую соответствующие строки
+    """
     # TODO make index names case insensitive
     # TODO нужно поменять сигнатуру этой функции - чтение будет из байтов
     # TODO также нужно убрать чтение из файла, либо просто добавить еще одну функцию - чтения байтов
     indexes = [] if not indexes else indexes
-    index = defaultdict(list)
+    # index = defaultdict(list)
+    index = {}
     with open(path) as f:
         rows = csv.reader(f)
         headers = get_headers(rows)
         indexes = check_index_names_and_get_known(indexes, headers, silent=True)
-        index_to_position = map_index_to_its_position(indexes, headers)
+        name_to_position = map_column_name_to_its_position(indexes, headers)
+        name_to_position_for_rows = map_column_name_to_its_position(flatten_headers(headers), headers)
         for row in rows:
             for index_ in indexes:
-                pos = index_to_position[index_]
-                index[row[pos]].append(row)
-    return index
+                pos = name_to_position[index_]
+                # index[row[pos]].append(process_row(row, name_to_position_for_rows))
+                index[row[pos]] = process_row(row, name_to_position_for_rows)
+    return index, filter(lambda x: x != '', flatten_headers(headers))
+
+
+path = '/Users/ivanmikhailov/Downloads/Таблица для бота - Бюджет v2.0 (копия).csv'
+d, h = build_index_on_raw_file_data(path, ['Дата'])
+for h_ in h:
+    print(h_, ':', d['05.02.2020'][h_])
+# .replace('\xa0', '').replace(' ₽', '') - хз поможет ли это, когда числа в экселике будут еще больше
