65119b10b67776f1006bdd7dd6154e4b2455dd19
Ivan Mikhailov <ivanmikhailov@Ivans-MacBook-Pro.local>
2021-07-14 22:56:04 +0300

    fix readme

--- main.py
+++ main.py
@@ -3,4 +3,6 @@ from status_pod.instagram.main import launch
 
 # NOTE прокрутка списка подписчиков плохо работает
 # TODO пофиксить прокрутку списка подписчиков и подписок
-launch()
+
+if __name__ == '__main__':
+    launch()
