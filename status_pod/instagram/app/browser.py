5fdb812153d83dce46edf161a9abbd570ed0723b
ivanmikhailov <mikhailovj@gmail.com>
2022-01-17 18:28:26 +0300

    fix browser config

--- status_pod/instagram/app/browser.py
+++ status_pod/instagram/app/browser.py
@@ -1,11 +1,5 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
 from status_pod.app.config import config
-
-
-# opt = Options()
-# if config.HEADLESS_MODE:
-#     opt.add_argument("--headless")
 
 chrome = webdriver.Chrome(executable_path=config.CHROME_DRIVER_PATH)
 
